0ce4b8d0b318c3023815d329f8024d892dce6158
Lacyus <lacyus@gmail.com>
2018-12-20 16:01:03 -0500

    additional

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
     var questionsNAnswers = [
         {
@@ -12,36 +12,164 @@ $(document).ready(function (){
             question: "What's the first Pokemon to be trademarked?",
             choices: ["Pikachu", "Mew", "Eevee", "Charmander"],
             answer: 1,
-            image: "assets/images/mew.gif"   
+            image: "assets/images/mew.gif"
         },
 
         {
             question: "What is the name of the summer camp in the Friday the 13th movie series?",
             choices: ["Camp Ruby Lake", "Camp Emerald Lake", "Camp Lake Lake", "Camp Crystal Lake"],
             answer: 3,
-            image: "assets/images/camp.jpeg"   
+            image: "assets/images/camp.jpeg"
         },
 
         {
             question: "Which artist's song was the 10 billionth iTunes download",
             choices: ["Drake", "Johnny Cash", "Jay-Z", "Lady Gaga"],
             answer: 1,
-            image: "assets/images/jhonny.gif"   
+            image: "assets/images/jhonny.gif"
         },
 
         {
             question: "What country does the cappuccino originate from?",
             choices: ["Britain", "France", "Italy", "Spain"],
             answer: 2,
-            image: "assets/images/italy.jpeg"   
-        },
-    ]
+            image: "assets/images/italy.jpeg"
+        }];
 
-    var timer = 30;
+    var timer = 25;
     var correct = 0;
     var wrong = 0;
-    var userPick ="";
+    var unanswered = 0;
+
+    var userPick = "";
     var newArray = [];
     var holder = [];
 
-};
+    var running = false;
+    var questionCount = questionsNAnswers.length;
+    var intervalId;
+    var pick;
+
+    
+    $("#reset").hide();
+    // Game start
+    $("#start").on("click", function () {
+        $("#start").hide();
+        displayQuestion();
+        runTimer();
+        for (var i = 0; i < questionsNAnswers.length; i++) {
+            holder.push(questionsNAnswers[i]);
+        }
+        // starts the timer
+    function runTimer() {
+        if (!running) {
+            intervalId = setInterval(decrement, 1000);
+            running = true;
+         }
+    }
+
+        //timer countdown
+    function decrement() {
+        $("#timer").html("<h3>Time remaining: " + timer + "</h3>");
+        timer--;
+
+         //when timer reaches 0
+        if (timer === 0) {
+            unanswered++;
+            stop();
+            $("#answers").html("<p>Time is up! The correct answers is: " + pick.choices[pick.answer] + "</p>");
+            hidepicture();
+        }
+    }
+
+    //timer stop
+    function stop() {
+        running = false;
+        clearInterval(intervalId);
+    }
+
+    function displayQuestion() {
+        //picks a random question from the array
+        index = Math.floor(Math.random() * questionsNAnswers.length);
+        pick = questionsNAnswers[index];
+
+
+        //iterate through answers array and display
+        $("#questions").html("<h2>" + pick.question + "</h2>");
+        for (var i = 0; i < pick.choices.length; i++) {
+             var userPick = $("<div>");
+             userPick.addClass("answerchoice");
+             userPick.html(pick.choices[i]);
+            //array position for answers check
+             userPick.attr("data-guessvalue", i);
+            $("#answers").append(userPick);
+                
+            }
+        }
+
+        $(".answerchoice").on("click", function () {
+            //grab array position from userPick
+            userPick = parseInt($(this).attr("data-guessvalue"));
+
+            //correct guess or wrong guess outcomes
+            if (userPick === pick.answer) {
+                stop();
+                correct++;
+                userPick = "";
+                $("#answers").html("<p>Correct!</p>");
+                hidepicture();
+
+            } else {
+                stop();
+                wrong++;
+                userPick = "";
+                $("#answers").html("<p>Wrong! The correct answer is: " + pick.choices[pick.answer] + "</p>");
+                hidepicture();
+            }
+        })
+    })
+
+    function hidepicture() {
+        $("#answers").append("<img src=" + pick.photo + ">");
+        newArray.push(pick);
+        questionsNAnswers.splice(index, 1);
+
+        var hidpic = setTimeout(function () {
+            $("#answers").empty();
+            timer = 5;
+
+            //run the score screen if all questions answered
+            if ((wrong + correct + unanswered) === questionCount) {
+                $("#questions").empty();
+                $("#questions").html("<h3>Game Over!  Here's the results: </h3>");
+                $("#answers").append("<h4> Correct: " + correct + "</h4>");
+                $("#answers").append("<h4> Incorrect: " + wrong + "</h4>");
+                $("#answers").append("<h4> Unanswered: " + unanswered + "</h4>");
+                $("#reset").show();
+                correct = 0;
+                wrong = 0;
+                unanswered = 0;
+
+            } else {
+                runTimer();
+                displayQuestion();
+
+            }
+        }, 3000);
+
+
+    }
+
+    $("#reset").on("click", function () {
+        $("#reset").hide();
+        $("#answers").empty();
+        $("#questions").empty();
+        for (var i = 0; i < holder.length; i++) {
+            questionsNAnswers.push(holder[i]);
+        }
+        runTimer();
+        displayQuestion();
+
+    })
+
+});
